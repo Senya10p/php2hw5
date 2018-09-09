2a859507d3817830991fe133d2ee98a369d2412a
senya <fuss-fuss-fuss@yandex.ru>
2018-09-09 21:08:18 +0300

    second commit

--- App/Controllers/Article.php
+++ App/Controllers/Article.php
@@ -10,9 +10,13 @@ class Article extends \App\Controller
         if ( isset($_GET['id']) ) { //проверяем на существование
 
             $this->view = new \App\View();
+
             $this->view->article = \App\Models\Article::findById( $_GET['id'] );
+
+            if (false === $this->view->article) {
+                throw new \App\Error404Exception('Ошибка 404 - не найдено:(');
+            }
         }
         $this->view->display(__DIR__ . '/../../templates/article.php');
     }
-
 }
